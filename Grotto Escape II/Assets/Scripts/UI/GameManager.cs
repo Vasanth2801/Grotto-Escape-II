25cf0262b287d2ecf24b6322cbe1c695b12f1c22
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-03-23 18:57:31 +0530

    All Canvas for the Game

---
+++ Grotto Escape II/Assets/Scripts/UI/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Grotto Escape II/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    public GameObject gameOver;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GameOver()
+    {
+        gameOver.SetActive(true);
+    }
+}
